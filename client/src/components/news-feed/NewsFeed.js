8e9677b1ccf2e9b6d462b488670436455ee264b8
Richard Eldridge <eldridge.rich@gmail.com>
2019-05-13 01:51:18 -0400

    change conditional in newsfeed component

--- client/src/components/news-feed/NewsFeed.js
+++ client/src/components/news-feed/NewsFeed.js
@@ -40,36 +40,42 @@ class NewsFeed extends Component {
       'Questions',
       'Other'
     ];
-    return (
-      <Container className="pb-3">
-        <Row>
-          <Col lg={3} className="sidebar mb-3">
-            <p className="lead mt-2">Filter posts by category</p>
-            {categories.map(category => (
-              <Button
-                key={category}
-                className="sidebar-button"
-                onClick={this.sortByCategory.bind(this, category)}
-              >
-                {category}
-              </Button>
-            ))}
-          </Col>
-          <Col lg={9} className="news-feed">
-            <CreatePost />
-            <Card className="mt-4 mb-2 p-2 bg-primary text-light shadow-sm text-center feed-header">
-              <h3>Project Village Feed</h3>
-            </Card>
-            {/* Posts go here */}
-            {posts && this.state.sorted
-              ? posts
-                  .filter(post => post.categories.includes(this.state.category))
-                  .map(post => <Post key={post._id} post={post} />)
-              : posts.map(post => <Post key={post._id} post={post} />)}
-          </Col>
-        </Row>
-      </Container>
-    );
+    if (posts) {
+      return (
+        <Container className="pb-3">
+          <Row>
+            <Col lg={3} className="sidebar mb-3">
+              <p className="lead mt-2">Filter posts by category</p>
+              {categories.map(category => (
+                <Button
+                  key={category}
+                  className="sidebar-button"
+                  onClick={this.sortByCategory.bind(this, category)}
+                >
+                  {category}
+                </Button>
+              ))}
+            </Col>
+            <Col lg={9} className="news-feed">
+              <CreatePost />
+              <Card className="mt-4 mb-2 p-2 bg-primary text-light shadow-sm text-center feed-header">
+                <h3>Project Village Feed</h3>
+              </Card>
+              {/* Posts go here */}
+              {this.state.sorted
+                ? posts
+                    .filter(post =>
+                      post.categories.includes(this.state.category)
+                    )
+                    .map(post => <Post key={post._id} post={post} />)
+                : posts.map(post => <Post key={post._id} post={post} />)}
+            </Col>
+          </Row>
+        </Container>
+      );
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 
