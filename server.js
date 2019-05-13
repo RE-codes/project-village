ad2839b37f3273bd9e984c4ec547d2ff9f5002fe
Richard Eldridge <eldridge.rich@gmail.com>
2019-05-13 00:59:00 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -22,6 +22,19 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/posts', require('./routes/posts'));
 // app.use('/api/profiles', require('./routes/profiles'));
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
